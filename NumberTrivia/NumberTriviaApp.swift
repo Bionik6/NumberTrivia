0c57a8d644638b0e7e4e42acd6dfb82bc50bdfe8
Ibrahima Ciss <bionik6@gmail.com>
2021-04-21 14:52:08 +0000

    Initial Commit

---
+++ NumberTrivia/NumberTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberTriviaApp.swift
+//  NumberTrivia
+//
+//  Created by Ibrahima Ciss on 21/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct NumberTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6e1a4a2a0d2f431b107da8a40c1d717881fc361f
Ibrahima Ciss <bionik6@gmail.com>
2021-04-24 18:18:46 +0000

    Refactor 'NumberTriviaRepositoryImplementation'

--- NumberTrivia/Data/Repositories/NumberTriviaRepositoryImplementation.swift
+++ NumberTrivia/Data/Repositories/NumberTriviaRepositoryImplementation.swift
@@ -1,18 +1,18 @@
-//
-//  NumberTriviaRepositoryImplementation.swift
-//  NumberTrivia
-//
-//  Created by Ibrahima Ciss on 23/04/2021.
-//
-
-import Foundation
-
-final class NumberTriviaRepositoryImplementation: NumberTriviaRepository {
-
+ //
+ //  NumberTriviaRepositoryImplementation.swift
+ //  NumberTrivia
+ //
+ //  Created by Ibrahima Ciss on 23/04/2021.
+ //
+ 
+ import Foundation
+ 
+ final class NumberTriviaRepositoryImplementation: NumberTriviaRepository {
+  
   private(set) var networkInfo: NetworkInfo
   private(set) var remoteDataSource: NumberTriviaRemoteDataSource
   private(set) var localDataSource: NumberTriviaLocalDataSource
-
+  
   init(networkInfo: NetworkInfo,
        remoteDataSource: NumberTriviaRemoteDataSource,
        localDataSource: NumberTriviaLocalDataSource)
@@ -21,25 +21,25 @@ final class NumberTriviaRepositoryImplementation: NumberTriviaRepository {
     self.localDataSource = localDataSource
     self.remoteDataSource = remoteDataSource
   }
-
+  
   func getRandomNumberTrivia(completion: @escaping NumberTriviaResponse) {
-    guard networkInfo.isConnected else {
-      localDataSource.getLastNumberTrivia { response in
-        if case .success(let model) = response { completion(.success(NumberTrivia(number: model.number, text: model.text))) }
-        if case .failure(let error) = response { if error == .noDataPresent { completion(.failure(.noCacheDataPresent)) } }
-      }
-      return
-    }
-    remoteDataSource.getRandomNumberTrivia { response in
-      if case .success(let model) = response {
-        self.localDataSource.cache(numberTrivia: model) { }
-        completion(.success(NumberTrivia(number: model.number, text: model.text)))
-      }
-      if case .failure = response { completion(.failure(.basic)) }
+    getNumberTrivia(completion: completion) {
+      remoteDataSource.getRandomNumberTrivia { self.handle(response: $0, completion: completion) }
     }
   }
-
+  
   func getConcreteNumberTrivia(number: Double, completion: @escaping NumberTriviaResponse) {
+    getNumberTrivia(completion: completion) {
+      remoteDataSource.getConcreteNumberTrivia(number: number) { self.handle(response: $0, completion: completion) }
+    }
+  }
+  
+ }
+ 
+ 
+ extension NumberTriviaRepositoryImplementation {
+  
+  private func getNumberTrivia(completion: @escaping NumberTriviaResponse, block: ()->()) {
     guard networkInfo.isConnected else {
       localDataSource.getLastNumberTrivia { response in
         if case .success(let model) = response { completion(.success(NumberTrivia(number: model.number, text: model.text))) }
@@ -47,13 +47,15 @@ final class NumberTriviaRepositoryImplementation: NumberTriviaRepository {
       }
       return
     }
-    remoteDataSource.getConcreteNumberTrivia(number: number) { response in
-      if case .success(let model) = response {
-        self.localDataSource.cache(numberTrivia: model) { }
-        completion(.success(NumberTrivia(number: model.number, text: model.text)))
-      }
-      if case .failure = response { completion(.failure(.basic)) }
-    }
+    block()
   }
-
-}
+  
+  private func handle(response: Result<NumberTriviaModel, NumberTriviaResponseError>, completion: @escaping NumberTriviaResponse) {
+    if case .success(let model) = response {
+      self.localDataSource.cache(numberTrivia: model) { }
+      completion(.success(NumberTrivia(number: model.number, text: model.text)))
+    }
+    if case .failure = response { completion(.failure(.basic)) }
+  }
+  
+ }
